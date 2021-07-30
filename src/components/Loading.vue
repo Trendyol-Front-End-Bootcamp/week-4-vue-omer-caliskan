383f277299becf7e20c0e434a3fb87a218e35d88
omerclskn <omercaliskan99@gmail.com>
2021-07-30 18:53:50 +0300

    add loading circle

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,15 +1,47 @@
 <template>
-    <h2>Loading...</h2>
+    <div class="load"></div>
 </template>
 
 <style scoped>
 
-h2{
-    height: 100px;
-    width: 100%;
-    color: #ffe81f;
-    text-align: center;
-    padding: 150px
+ @keyframes rotate {
+    from {
+        transform: rotate(0deg);
+    }
+    to { 
+        transform: rotate(360deg);
+    }
+}
+ 
+
+ @-webkit-keyframes rotate {
+    from {
+        -webkit-transform: rotate(0deg);
+    }
+    to { 
+        -webkit-transform: rotate(360deg);
+    }
+}
+
+.load {
+	width: 100px;
+	height: 100px;
+	margin: 250px auto 0;
+	border:solid 10px #FFE81F;
+	border-radius: 50%;
+	border-right-color: transparent;
+	border-bottom-color: transparent;
+	 -webkit-transition: all 0.5s ease-in;
+    -webkit-animation-name:             rotate; 
+    -webkit-animation-duration:         1.0s; 
+    -webkit-animation-iteration-count:  infinite;
+    -webkit-animation-timing-function: linear;
+    	
+    	 transition: all 0.5s ease-in;
+    animation-name:             rotate; 
+    animation-duration:         1.0s; 
+    animation-iteration-count:  infinite;
+    animation-timing-function: linear; 
 }
 
 </style>
